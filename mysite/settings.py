c5dabcb0c10daf3e4880d9f7fe43c949ec9cdb3f
zigy <solo.games.9@hotmail.com>
2020-11-19 14:32:47 -0300

    add requeriments

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.sites',
     #Local Apps
     'blog.apps.BlogConfig',
     #third apps
